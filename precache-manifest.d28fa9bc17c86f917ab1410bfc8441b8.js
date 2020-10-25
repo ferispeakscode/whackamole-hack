0ecebad948ed712b4a6d80f93877e7e1263df458
ferispeakscode@gmail.com <ferispeakscode@gmail.com>
2020-10-25 15:24:25 -0400

    Updates

---
+++ precache-manifest.d28fa9bc17c86f917ab1410bfc8441b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7161dca44105f20574f1fe1da0f75a0",
+    "url": "/whackamole-hack/index.html"
+  },
+  {
+    "revision": "839611ae55e6412f25ef",
+    "url": "/whackamole-hack/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "f9782a8366d8c7532156",
+    "url": "/whackamole-hack/static/js/2.680e9bc6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/whackamole-hack/static/js/2.680e9bc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "839611ae55e6412f25ef",
+    "url": "/whackamole-hack/static/js/main.3bf471cf.chunk.js"
+  },
+  {
+    "revision": "bbe90b6e31a46b9c3b11",
+    "url": "/whackamole-hack/static/js/runtime-main.9b1b7d45.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/whackamole-hack/static/media/mintbean.cf18b629.png"
+  }
+]);
