237e351dee2f2ea9d40e3bd007a60dfb335b1b22
ferispeakscode@gmail.com <ferispeakscode@gmail.com>
2020-10-26 22:38:17 -0400

    Created start button component

---
+++ src/components/StartComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './styles/index.css';
+
+function Start() {
+  return (
+    <div className="">
+        <button>Start Game</button>
+    </div>
+  );
+}
+
+export default Start;
