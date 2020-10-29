6cd90987a2a532be844db3a17ec81c38bca001e2
ferispeakscode@gmail.com <ferispeakscode@gmail.com>
2020-10-28 23:27:44 -0400

    Added checkMole function to squares to check for moles vs. non-moles

--- src/components/SquareComponent.js
+++ src/components/SquareComponent.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 import '../styles/square.css';
 
 class Square extends Component {
+    constructor (props) {
+        super();
+        this.state = {
+            value: props.value,
+            mole: "あ"
+        }
+    }
+
+    checkMole = () => {
+        if ( this.state.value == this.state.mole) {
+            console.log("You whacked the mole!");
+        }
+        else {
+            console.log("Oops, try again!");
+        }
+    }
+
     render() {
         return (
-            <button className="square">
+            <button className="square" onClick={this.checkMole}>
                 {this.props.value}
             </button>
         );
